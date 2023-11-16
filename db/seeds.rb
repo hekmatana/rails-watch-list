5b516f4e059c6a0a75b8b19c27b3663e7effcae9
Anahita Hekmat <hekmatana@gmail.com>
2023-11-16 11:41:12 +0100

    controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,23 @@
 #   end
 
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+require 'faker'
+
+50.times do
+
+
+
+
+
+  movie = Movie.new(
+    title: ,
+    overview: ,
+    poster_url: ,
+    rating: rand(1..100)/10,
+  )
+  article = Article.new(
+    title: Faker::Ancient.primordial,
+    content: Faker::Markdown.emphasis
+  )
+  article.save!
+end
